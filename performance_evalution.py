06c5924ad362f7390b6571cad07e620afa72d1f0
damianxiao <damianxiao22@gmail.com>
2023-04-12 11:30:18 +0800

    2

--- performance_evalution.py
+++ performance_evalution.py
@@ -1,23 +1,33 @@
 import threading
 import time
 import hashlib
-import time
+import os
 
 start_time = time.time()
 
 class Node:
-    def __init__(self, port, difficulty):
+    def __init__(self, port, difficulty,cpu_limit):
         self.port = port
         self.peers = []
         self.messages = []
         self.difficulty = difficulty
         self.shares = None
         self.nonce = 0
+        self.cpu_limit = cpu_limit
+        self.is_evaluating = False
+        
+        if port == 5000:  # 如果是节点1，则将CPU限制设为 None，即不限制
+            self.cpu_limit = None
+        else:
+            self.cpu_limit = cpu_limit
+        self.is_evaluating = False
         
         # 创建cgroup并设置CPU限制
         cg_name = f"node{port}_cg"
-        os.system(f"sudo cgcreate -g cpu:{cg_name}")
-        os.system(f"sudo cgset -r cpu.cfs_quota_us={cpu_limit} {cg_name}")
+        os.system(f"C:/cygwin64/bin/bash.exe -l -c 'cgcreate -t $(whoami) -a $(whoami) -g cpu:{cg_name}'")
+        os.system(f"C:/cygwin64/bin/bash.exe -l -c 'cgset -r cpu.cfs_quota_us={cpu_limit} {cg_name}'")
+        # os.system(f"runas cgcreate -g cpu:{cg_name}")
+        # os.system(f"runas cgset -r cpu.cfs_quota_us={cpu_limit} {cg_name}")
 
     def add_peer(self, peer):
         if peer not in self.peers:
@@ -35,11 +45,12 @@ class Node:
         self.messages.append((sender, message))
 
     def run(self):
+        self.start_evaluation()
         while True:
             # 模拟发送消息
             message = "node {}: shares {}".format(self.port, self.shares)
             self.broadcast(message)
-            print("{} :{}".format(self.port, message))
+            # print("{} :{}".format(self.port, message))
             time.sleep(2)
 
     def calculate_shares(self, nonce):
@@ -61,17 +72,20 @@ class Node:
                 message = f"Node {self.port} has generated {shares} shares"
                 self.broadcast(message) # 广播产生新的shares信息
             if time.time() - start_time >= 5: # 5秒内产生的share
-                print(f"Node {self.port} Nonce: {nonce}, Shares: {shares}")
-                print(f"hash_int={hash_int} target={target}")
+                # print(f"Node {self.port} Nonce: {nonce}, Shares: {shares}")
+                # print(f"hash_int={hash_int} target={target}")
                 break
         return shares
 
     def evaluate_node_performance(self):
-        os.system(f"sudo cgclassify -g cpu:node{self.port}_cg {os.getpid()}")
-        nonce = 0
-        shares = self.calculate_shares(nonce)
-        print(f"Node {self.port} shares: {shares}")
-        self.shares = shares
+        if not self.is_evaluating:
+            self.is_evaluating = True
+            os.system(f"C:/cygwin64/bin/bash.exe -l -c 'cgclassify -g cpu:node{self.port}_cg {os.getpid()}'")
+            # os.system(f"runas cgclassify -g cpu:node{self.port}_cg {os.getpid()}")
+            nonce = 0
+            shares = self.calculate_shares(nonce)
+            print(f"Node {self.port} shares: {shares}")
+            self.shares = shares
     
     def start_evaluation(self):
         thread = threading.Thread(target=self.evaluate_node_performance)
@@ -80,21 +94,21 @@ class Node:
 if __name__ == "__main__":
     
     nodes = [
-        {"id": 1, "port": 5000, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 50000},
-        {"id": 2, "port": 5001, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 75000},
-        {"id": 3, "port": 5002, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 25000},
-        {"id": 4, "port": 5003, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 30000},
-        {"id": 5, "port": 5004, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 40000},
-        {"id": 6, "port": 5005, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 20000},
-        {"id": 7, "port": 5006, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 15000},
-        {"id": 8, "port": 5007, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 80000},
-        {"id": 9, "port": 5008, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 60000},
-        {"id": 10, "port": 5009, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 35000}
+        {"id": 1, "port": 5000, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 5000000000},
+        {"id": 2, "port": 5001, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 7005000},
+        {"id": 3, "port": 5002, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 2005000},
+        {"id": 4, "port": 5003, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 3000000},
+        {"id": 5, "port": 5004, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 1},
+        {"id": 6, "port": 5005, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 1},
+        {"id": 7, "port": 5006, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 1},
+        {"id": 8, "port": 5007, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 1},
+        {"id": 9, "port": 5008, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 1},
+        {"id": 10, "port": 5009, "difficulty": "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "cpu_limit": 1}
         ]
 
     node_objects = []
     for node in nodes:
-        node_object = Node(node["port"], node["difficulty"])
+        node_object = Node(node["port"], node["difficulty"], node["cpu_limit"])
         node_objects.append(node_object)
 
     # 将节点互相连接
@@ -108,9 +122,9 @@ if __name__ == "__main__":
         thread = threading.Thread(target=node.run)
         threads.append(thread)
         thread.start()
-        node.start_evaluation()
+        # node.start_evaluation()
 
     while True:
-        time.sleep(1)
+        time.sleep(5)
         # for node in node_objects:
             # print(f"Node {node.port} shares: {node.shares}")
